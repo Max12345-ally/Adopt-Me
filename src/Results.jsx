0bc1686c26934c27cec338debdce0fa8e74dfa14
maxbond <ceo@rf.ru>
2023-03-19 23:45:27 +0300

    components added2

---
+++ src/Results.jsx
@@ -0,0 +1,22 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+    return (
+        <div className="search">
+        {!pets.length ? (
+            <h1>No pets found</h1>
+        ) : (
+            pets.map((pet => (
+                <Pet 
+                animal={pet.animal}
+                name={pet.name}
+                breed={pet.breed}
+                images={pet.images}
+                key={pet.id}
+                />
+            ))
+          ))}  
+        </div>
+    )
+}
+export default Results;
