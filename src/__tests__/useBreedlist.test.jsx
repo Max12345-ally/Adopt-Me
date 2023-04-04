bba7e55cc055988de4f3d4e2fb99b8b5e0b0955b
maxbond <ceo@rf.ru>
2023-04-04 19:50:53 +0300

    4 test passed

--- src/__tests__/useBreedlist.test.jsx
+++ src/__tests__/useBreedlist.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { render } from "@testing-library/react";
+import { render, renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
 
@@ -33,4 +33,32 @@ test("gimes an empty array list with no animal provided", async () => {
     const [breedList, status] = getBreedList();
     expect(breedList).toHaveLength(0);
     expect(status).toBe("loading");
+});
+
+test("gives back breeds when given an animal", async () => {
+    const breeds = [
+        "Havanese",
+        "Bichon Frise",
+        "Poodle",
+        "Maltese",
+        "Golden Retriever",
+        "Labrador",
+        "Husky",
+    ];
+    fetch.mockResponseOnce(
+        JSON.stringify({
+        animal: "dog",
+        breeds
+        })
+    );
+    const { result } = renderHook(() => useBreedList("dog"), {
+        wrapper: ({ children }) => (
+            <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        ),
+    });
+
+    await waitFor(() => expect(result.current[1]).toBe("success"))
+
+    const [breedlist] = result.current;
+    expect(breedlist).toEqual(breeds);
 });
